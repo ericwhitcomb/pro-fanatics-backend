c30f11543f6cda500a788e98e4fe5cfdb685c13f
Eric Whitcomb <eric.whitcomb@gmail.com>
2020-01-29 01:05:17 -0700

    Set up postgres connection with knex, users table added with seeds

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 
+const userModel = require('./db/models/userModel');
+
 const app = express();
 
 app.use(express.json());
@@ -9,7 +11,15 @@ app.use(cors());
 const PORT = process.env.PORT || '4000';
 
 app.get('/', (req, res) => {
-  res.status(200).send(`API active on port: ${PORT}`);
+    userModel
+        .findAllUsers()
+        .then(users => {
+            console.log(users);
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Server error' });
+        });
+    res.status(200).send(`API active on port: ${PORT}`);
 });
 
 module.exports = app;
