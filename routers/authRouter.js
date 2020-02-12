af983e0141a77dd28a3c1fce06fa8de178934ab9
Eric Whitcomb <eric.whitcomb@gmail.com>
2020-02-12 16:20:56 -0700

    Added console logs to troubleshoot login issue on heroku

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -7,19 +7,27 @@ const userModel = require('../db/models/userModel');
 const router = express.Router();
 
 router.post('/login', (req, res) => {
+    console.log('/api/auth/login');
     const creds = req.body;
+    console.log(creds);
     userModel
         .findByEmail(creds.email)
         .then(user => {
+            console.log('findByEmail: then');
             if (user && bcrypt.compareSync(creds.password, user.password)) {
+                console.log('findByEmail: then if');
                 delete user['password'];
+                console.log('findByEmail: then if a');
                 const token = authHelper.generateToken(user);
+                console.log('findByEmail: then if b');
                 res.json({ token });
             } else {
+                console.log('findByEmail: then else');
                 res.status(401).json({ message: 'Invalid email or password' });
             }
         })
         .catch(err => {
+            console.log('findByEmail: catch');
             res.status(500).json({ message: 'Server error' });
         });
 });
